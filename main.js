b0d66e5b00130d46057d32afe3dfd2e2b7448c00
Zak Bay <zakbay@Nancys-MacBook-Pro.local>
2022-11-19 02:30:26 -0800

    Hero

--- main.js
+++ main.js
@@ -2,23 +2,26 @@ const modalOpen = "[data-open]";
 const modalClose = "[data-close]";
 const isVisible = "is-visible"
 
+
+//// -------------------- MODAL STUFF ------------------------ //////
+
 // this will store any element that has "[data-open]" in a node list 
 const openModal = document.querySelectorAll(modalOpen);
 
 // this will store any element that has "[data-close]" in a node list 
 const closeModal = document.querySelectorAll(modalClose);
 
-// for of loop to iterate through node list of queried elements and adding eventlister to each one 
-for (const elm of openModal) {
+// for of loop to iterate through node list of queried elements above 
+//  and adding eventlistener to each one 
+
+for (const elm of openModal) { // open Modal buttons
    elm.addEventListener('click', function() {
-      const modalId = this.dataset.open;  // "this" is referring to the ELM
+      const modalId = this.dataset.open;  // "this" is referring to the parent Element
       document.getElementById(modalId).classList.add(isVisible)
    })
 }
 
-
-// close modal buttons 
-for (const elm of closeModal) {
+for (const elm of closeModal) { // close modal buttons 
    elm.addEventListener('click', function() {
       this.parentElement.parentElement.classList.remove(isVisible)
    })
